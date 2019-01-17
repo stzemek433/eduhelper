d2e869e2fd141e457aa68b4904358605077d560c
krystofpiorecky <37453841+krystofpiorecky@users.noreply.github.com>
2019-01-17 20:18:07 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,153 @@
+let answersToQuestions = [
+	{
+		q: "Bezeztrátová komprese (2 správně)",
+		a: [
+			"komprimovaný soubor lze opačným postupem rekonstruovat do původní podoby",
+			"má nižší kompresní poměr než ztrátová"
+		]
+	},
+	{
+		q: "Soubory MPEG layer 2 mají koncovku:",
+		a: [
+			"*.mp2"
+		]
+	},
+	{
+		q: "MPEG layer 3 využívá",
+		a: [
+			"model Temporal Masking"
+		]
+	},
+	{
+		q: "Datový tok MPEG layer1 má rychlost",
+		a: [
+			"od 32 kb/s do 448 kb/s"
+		]
+	},
+	{
+		q: "MPEG layer2 má datový tok:",
+		a: [
+			"od 32 kb/s do 384 kb/s"
+		]
+	},
+	{
+		q: "Komprese  ztrátového modelu, která je založena na principu vynechání detailů, jež jsou pro lidské ucho nepostižitelné, se nazývá: (vyber 2)",
+		a: [
+			'"perceptual noise shaping" v oblasti dynamického rozsahu',
+			'"perceptual subband transform coding" v oblasti frekvenčního rozsahu'
+		]
+	},
+	{
+		q: "Úkolem komprese dat je: (2 správně)",
+		a: [
+			"zmenšit potřebný úložný prostor",
+			"zmenšit datový tok"
+		]
+	},
+	{
+		q: "MPEG layer 2 pracuje s metodou",
+		a: [
+			"*.mp2"
+		]
+	},
+	{
+		q: "Bezeztrátová komprese (2 správně)",
+		a: [
+			"komprimovaný soubor lze opačným postupem rekonstruovat do původní podoby",
+			"má nižší kompresní poměr než ztrátová"
+		]
+	},
+	{
+		q: "MPEG layer 2 pracuje s metodou",
+		a: [
+			"Temporal Masking"
+		]
+	},
+	{
+		q: "Kompresní poměr MPEG layer 3 při CD kvalitě dosahuje:",
+		a: [
+			"12:1"
+		]
+	},
+	{
+		q: "Co je to kodek? (vyber dvě)",
+		a: [
+			"zařízení, které transformuje datový proud",
+			"program, který transformuje datový proud"
+		]
+	},
+	{
+		q: "Při kompresi s běžnou rychlostí vzorkování (128kbps) signálu má:",
+		a: [
+			"soubor mp3 přibližně 10x menší velikost, než soubor WAV"
+		]
+	},
+	{
+		q: "Datový tok MPEG layer 3 je",
+		a: [
+			"32 kb/s do 320 kb/s"
+		]
+	},
+	{
+		q: "Ztrátová komprese (vyber dvě)",
+		a: [
+			"informace nelze kompletně rekonstruovat",
+			"používá se pro kompresi zvuku a obrazu"
+		]
+	},
+	{
+		q: "MPEG layer 2 pracuje",
+		a: [
+			"se třemi frame najednou"
+		]
+	},
+	{
+		q: "Proces komprese (vyber dvě)",
+		a: [
+			"může provádět specielní hardware",
+			"zatěžuje procesor počítače"
+		]
+	},
+	{
+		q: "MPEG layer1 metoda pracuje",
+		a: [
+			"s jedním frame najednou"
+		]
+	},
+	{
+		q: "MPEG layer 3 využívá",
+		a: [
+			"redukce vazeb mezi stereo signály"
+		]
+	},
+	{
+		q: "Layer 1 formátu MPEG využívá při kompresi zvuku model:",
+		a: [
+			"pouze Frequency Masking"
+		]
+	},
+	{
+		q: "Při kompresi signálu bílého šumu má frekvenčí spektrum",
+		a: [
+			"utlumené vysoké frekvence"
+		]
+	},
+	{
+		q: "Kompresní poměr je:",
+		a: [
+			"poměr velikosti dat před a po kompresi"
+		]
+	},
+	{
+		q: 'Metoda komprese "Temporal Masking" je založena na jevu',
+		a: [
+			"setrvačnost vjemu zvuku"
+		]
+	},
+	{
+		q: 'Metoda komprese "Frequency Masking" využívá jevu:',
+		a: [
+			"lidské ucho není schopno rozlišit v přítomnosti silného signálu signál slabší"
+		]
+	}
+];
